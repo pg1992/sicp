5b064da9123ac6be041ee5151ca050be33c0474f
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-04-11 11:17:09 -0300

    Move subprocedures definition inside sqrt definition (lexical scoping)

--- newton-sqrt.scm
+++ newton-sqrt.scm
@@ -1,22 +1,16 @@
 (define (sqrt x)
-        (sqrt-iter 1.0 x))
-
-(define (sqrt-iter guess x)
-        (if (good-enough? guess x)
-            guess
-            (sqrt-iter (improve guess x) x)))
-
-(define (improve guess x)
-        (average guess (/ x guess)))
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
 
 (define (average x y)
-        (/ (+ x y) 2))
-
-(define (good-enough? guess x)
-        (< (abs (- x (square guess))) 0.0001))
-
-(define (square x)
-        (* x x))
+  (/ (+ x y) 2))
 
 (sqrt 9)
 (sqrt (+ 100 37))
