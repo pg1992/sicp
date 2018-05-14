7fa80cb652e1a1c9d0f3db4dee2ad60e6bb62117
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-05-14 15:32:04 -0300

    Add fibonacci example (tree recursive and linear iterative)

---
+++ ch01/fibonacci.scm
@@ -0,0 +1,18 @@
+(define n 10)
+
+; tree-recursive
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+(fib n)
+
+; linear-iterative
+(define (fib n)
+  (define (iter a b count)
+    (if (= count 0)
+        b
+        (iter (+ a b) a (- count 1))))
+  (iter 1 0 n))
+(fib n)
